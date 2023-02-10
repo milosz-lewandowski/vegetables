b5bb25c286f079450dc31eb960e91c4227a6daed
Miłosz Lewandowski <milo.lewandowski@gmail.com>
2023-02-10 08:07:03 +0100

    Config

---
+++ src/main/java/com/miloszlewandowski/Application.java
@@ -0,0 +1,14 @@
+package com.miloszlewandowski;
+
+import com.miloszlewandowski.micronaut.VegetableCommand;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
